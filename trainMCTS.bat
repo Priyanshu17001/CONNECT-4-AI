8466838ea2c106c3a6b6392e43a1abcf98b18de7
Priyanshu Singh <47223652+Priyanshu17001@users.noreply.github.com>
2019-09-09 12:11:55 +0530

    Update trainMCTS.bat

--- trainMCTS.bat
+++ trainMCTS.bat
@@ -2,6 +2,6 @@
 FOR /L %%A IN (1,1,200) DO (
   REM Your-python-path\python.exe Path-For-your-file.py
   REM remove REM from previous line before executing
-  C:\ProgramData\Anaconda3\python.exe C:\Users\priyanshu\Desktop\trainMCTS.py
+  C:\ProgramData\Anaconda3\python.exe C:\Users\priyanshu\Desktop\trainingMCTS.py
 )
 PAUSE
